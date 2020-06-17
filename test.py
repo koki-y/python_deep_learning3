8663b7d3757799586028e9c24637f4bc395b0528
koki-y <koki-y@sra.co.jp>
2020-06-17 17:34:19 +0900

    Step13まで実装

--- test.py
+++ test.py
@@ -2,6 +2,12 @@ import unittest
 
 from variable import *
 
+class AddTest(unittest.TestCase):
+    def test_forward(self):
+        y = add(Variable(np.array(3)), Variable(np.array(4)))
+        expected = np.array(7)
+        self.assertEqual(y.data, expected)
+    
 class SquareTest(unittest.TestCase):
     def test_forward(self):
         x = Variable(np.array(2.0))
@@ -23,4 +29,16 @@ class SquareTest(unittest.TestCase):
         num_grad = numerical_diff(square, x)
         flg = np.allclose(x.grad, num_grad)
         self.assertTrue(flg)
+
+class CombinationTest(unittest.TestCase):
+    def test_squared_add(self):
+        x = Variable(np.array(2.0))
+        y = Variable(np.array(3.0))
+
+        z = add(square(x), square(y))
+        z.backward()
+
+        self.assertEqual(z.data, np.array(13))
+        self.assertEqual(x.grad, np.array(4))
+        self.assertEqual(y.grad, np.array(6))
 
--- variable.py
+++ variable.py
@@ -13,7 +13,6 @@ class Variable:
     def set_creator(self, func):
         self.creator = func
 
-## Backward in loop
     def backward(self):
         if self.grad is None:
            # set first gradient.
@@ -22,33 +21,33 @@ class Variable:
         funcs = [self.creator]
         while funcs:
             f = funcs.pop()
-            x, y = f.input, f.output
-            x.grad = f.backward(y.grad)
-            if x.creator is not None:
-               funcs.append(x.creator)
-
-## Backward recursively (Don't use)
-    def _backward(self):
-        f = self.creator
-        if f is not None:
-            x = f.input
-            x.grad = f.backward(self.grad)
-            x.backward()
+            xs, ys = f.inputs, f.outputs
+            gys = [ y.grad for y in ys ]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+            for x, gx in zip(xs, gxs):
+                x.grad = gx
+                if x.creator is not None:
+                    funcs.append(x.creator)
 
 def is_not_ndarray(data):
     return not isinstance(data, np.ndarray)
 
 class Function:
-    def __call__(self, input):
-        x = input.data
-        y = self.forward(x)
-        output = Variable(as_array(y))
-        output.set_creator(self)
-        self.input = input
-        self.output = output
-        return output
+    def __call__(self, *inputs):
+        xs = [ x.data for x in inputs ]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [ Variable(as_array(y)) for y in ys ]
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs if len(outputs) > 1 else outputs[0]
 
-    def forward(self, x):
+    def forward(self, xs):
         raise NotImplementedError()
 
     def backward(self, gy):
@@ -59,12 +58,19 @@ def as_array(x):
         return np.array(x)
     return x
 
+class Add(Function):
+    def forward(self, x0, x1):
+        return x0 + x1
+
+    def backward(self, gy):
+        return gy, gy
+
 class Square(Function):
     def forward(self, x):
         return x ** 2
 
     def backward(self, gy):
-        x = self.input.data
+        x = self.inputs[0].data
         gx = 2 * x * gy
         return gx
 
@@ -76,6 +82,9 @@ class Exp(Function):
         x = self.input.data
         gx = np.exp(x) * gy
         return gx
+
+def add(x0, x1):
+    return Add()(x0, x1)
 
 def square(x):
     return Square()(x)
