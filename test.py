206d9ed5db52881336f52164be0c4d6a9f35608c
koki-y <koki-y@sra.co.jp>
2020-07-07 18:46:28 +0900

    step24まで実装

--- test.py
+++ test.py
@@ -64,7 +64,7 @@ class DivTest(unittest.TestCase):
         self.assertEqual(a.grad, 0.5)
         self.assertEqual(b.grad, -1.0)
 
-class powTest(unittest.TestCase):
+class PowTest(unittest.TestCase):
     def test_forward(self):
         x = Variable(np.array(2.0))
         y = pow(x, 4)
@@ -206,4 +206,44 @@ class OperatorTest(unittest.TestCase):
 
         self.assertEqual(y.data, np.array(16.0))
         self.assertEqual(x.grad, 32.0)
+
+
+
+class ComplexFuctionTest(unittest.TestCase):
+    def test_sphere(self):
+        def sphere(x, y):
+            return x ** 2 + y ** 2
+
+        x = Variable(np.array(1.0))
+        y = Variable(np.array(1.0))
+        z = sphere(x, y)
+        z.backward()
+
+        self.assertEqual(x.grad, np.array(2.0))
+        self.assertEqual(y.grad, np.array(2.0))
+
+    def test_matyas(self):
+        def sphere(x, y):
+            return 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
+
+        x = Variable(np.array(1.0))
+        y = Variable(np.array(1.0))
+        z = sphere(x, y)
+        z.backward()
+
+        self.assertEqual(x.grad, np.array(0.040000000000000036))
+        self.assertEqual(y.grad, np.array(0.040000000000000036))
+
+    def test_goldstein(self):
+        def goldstein(x, y):
+            return (1 + (x + y + 1) ** 2 * (19 - 14 * x + 3 * x ** 2 - 14 * y + 6 * x * y + 3 * y **2)) * \
+                   (30 + (2 * x - 3 * y) ** 2 * (18 - 32 * x + 12 * x ** 2 + 48 * y -36 * x * y + 27 * y ** 2))
+
+        x = Variable(np.array(1.0))
+        y = Variable(np.array(1.0))
+        z = goldstein(x, y)
+        z.backward()
+
+        self.assertEqual(x.grad, np.array(-5376.0))
+        self.assertEqual(y.grad, np.array(8064.0))
 
