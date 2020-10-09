ea1097ef2b5c1a55de1c45e1be2fb9b4c5999d7e
koki-y <koki-y@sra.co.jp>
2020-10-09 14:35:45 +0900

    Step54まで実装

--- dezero/core.py
+++ dezero/core.py
@@ -7,6 +7,7 @@ import dezero
 
 class Config:
     enable_backprop = True
+    train = True
 
 try:
     import cupy as cp
@@ -219,6 +220,9 @@ def using_config(name, value):
         yield
     finally:
         setattr(Config, name, old_value)
+
+def test_mode():
+    return using_config('train', False)
 
 def no_grad():
     return using_config("enable_backprop", False)
--- dezero/functions.py
+++ dezero/functions.py
@@ -313,3 +313,14 @@ def sigmoid(x):
 def relu(x):
     return ReLU()(x)
 
+def dropout(x, dropout_ratio=0.5):
+    x = as_variable(x)
+    if dezero.Config.train:
+        xp = cuda.get_array_module(x)
+        mask = xp.random.rand(*x.shape) > dropout_ratio
+        scale = xp.array(1.0 - dropout_ratio).astype(x.dtype)
+        y = x * mask / scale
+        return y
+    else:
+        return x
+
